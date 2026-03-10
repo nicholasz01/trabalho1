3500a6ea18bb2b2726544b0223a5b9219cbd9bd9
nicholasz01 <nicholas.cardoso.santos@escola.pr.gov.br>
2026-03-10 09:14:12 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const botao = document.getElementById('btnSimular');
+(adventListener)
+botao.addEventListener('click',funtion({
+    input(getElemetById
+        const metodoSelecionado = document.getElementById()
+    )
+}))
